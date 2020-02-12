b7297588ab7ada745535d098461488bb44d10755
NoUrEdDiN <noureddin95@gmail.com>
2020-02-12 02:03:38 +0200

    improve the index file

--- mk.pl
+++ mk.pl
@@ -16,15 +16,16 @@ while (my $diafile = readdir($diadir)) {
 
     my $svgfile = $diafile=~s|dia$|svg|r;
     my $htmlfile = $svgfile=~s/svg$/html/r;
+
+    $htmlfiles{$htmlfile} = undef;
+
     $diafile = $diarootdir.$diafile;
-    my $barehtmlfile = $htmlfile;
     $htmlfile = $htmlrootdir.$htmlfile;
 
-    $htmlfiles{$barehtmlfile} = undef;
 
     my $title = $svgfile=~s|-|/|r =~ s/[.].*//r;
 
-    push @chartedfns, qq{<li><p><a href="$barehtmlfile">$title</a></p></li>}
+    push @chartedfns, $title
         if ($title !~ m|/|);
 
     # next unless $htmlfile is more recent than $diafile; ie, already converted.
@@ -40,7 +41,7 @@ while (my $diafile = readdir($diadir)) {
     $svgimg =~ s|(?<=width=")([.0-9]+)|$1*2/3|e;
     $svgimg =~ s|(?<=height=")([.0-9]+)|$1*2/3|e;
     # center the image vertically
-    my $topoffset = (19.89-$1*2/3)/2;  # 19.89cm emperically equals 720px
+    my $topoffset = (19.89-$1*2/3)/2;  # 19.89cm emperically equals 720px -- works on my machine :P
     $topoffset = 0 if $topoffset < 0;
 
     # slurp the rest of the svg file
@@ -90,18 +91,14 @@ while (my $diafile = readdir($diadir)) {
 }
 closedir $diadir;
 
-# remove all htmlfiles that are not in %htmlfiles; these are files that no longer
-# have corresponding $diafile, for example their $diafile is renamed or removed.
+# Remove all htmlfiles that are not in %htmlfiles; these are files that no longer
+#   have corresponding $diafile: for example their $diafile is renamed or removed.
 opendir my $htmldir, +$htmlrootdir;
 while (my $htmlfile = readdir($htmldir)) {
     next unless ($htmlfile =~ m/[.]html$/);
     unlink $htmlfile unless exists $htmlfiles{$htmlfile};
 }
 closedir $htmldir;
-
-my $boldp = '<p style="font-weight: bold">';
-my $chartedfns = join "\n      ", sort map { />main</? s/<p>/$boldp/r : $_ } @chartedfns;
-# bolding main this way has the nice side-effect of making it the first one when sorting
 
 open my $indexfh, '>', $htmlrootdir.'index.html';
 print {$indexfh} <<~"EOT";
@@ -117,34 +114,76 @@ print {$indexfh} <<~"EOT";
         table { /* center */ margin: 0 auto; border: none; }
         td:first-child { text-align: left; }
         td.def { text-align: right; font-size: 65%; }
+        a { text-decoration: none; color: #1034A6; } /* Egyptian Blue, b/c I'm Egyptian :P */
+        #main {
+          text-align: center;
+          font-weight: bold;
+          font-size: 150%;
+        }
+        hr { margin-top: 1rem; }
       </style>
     </head>
     <body>
       <h1>nDPI Flowcharts (Unofficial)</h1>
+      <div id="main"><a href="main.html">Start Here!</a></div>
+      <hr>
       <h2>Currently Charted Functions</h2>
-      <ul>
-        $chartedfns
-      </ul><hr>
+      <table>
+  EOT
+
+my $chartedfns = join '',
+    map { qq[      <tr><td><a href="$_->[0].html">$_->[0]</a></td><td class="def">defined in <b>$_->[1]</b></td></tr>\n] }
+    sort { $a->[1] cmp $b->[1] || $a->[0] cmp $b->[0] }
+    map { [ $_->[0], ($_->[1] =~ /^[.]/)? $_->[1] =~ s|^\.\.\/||r : 'example/'.$_->[1] ] }
+    grep { $_->[0] ne 'main' }
+    map { [split] }
+    map { `bash where-is-defined.sh $_` }
+        @chartedfns;
+
+# Probably the following is the worst way to have some vertical space between
+#   rows that contain functions that are defined in different source files.
+my $prevsource;
+for (split "\n", $chartedfns) {
+    /<b>([^<]+)</;
+
+    if (not defined $prevsource) {
+        $prevsource = $1;
+        print {$indexfh} $_, "\n";
+        next;
+    }
+
+    if ($prevsource ne $1) {
+        s|<tr>|<tr><td style="padding:5px"></td><td></td></tr>\n      <tr>|;
+        $prevsource = $1;
+    }
+
+    print {$indexfh} $_, "\n";
+}
+
+print {$indexfh} <<~"EOT";
+      </table><hr>
       <h2>Not-yet Charted Functions<br><span class="subheader">(That Are Linked From The Currently Charted Functions)</span></h2>
       <table>
   EOT
 
-my @missing = split "\n", `bash where-is-defined.sh`;
 print {$indexfh}
-    qq[        <tr><td>$_->[0]</td><td class="def">defined in <b>$_->[1]</b></td></tr>\n]
-        for map { [split] } @missing;
+    qq[      <tr><td>$_->[0]</td><td class="def">defined in <b>$_->[1]</b></td></tr>\n]
+        for
+            sort { $a->[1] cmp $b->[1] || $a->[0] cmp $b->[0] }
+            map { [split / +\.\.\// ] }
+                split "\n", `bash where-is-defined.sh`;
 
 my $repo = 'github.com/noureddin/ndpi-flowcharts';
 my $demo = 'noureddin.github.io/ndpi-flowcharts';
 my $orig = 'github.com/ntop/nDPI';
-print {$indexfh} <<"EOT";
+print {$indexfh} <<~"EOT";
       </table><hr>
       <p>Online Repo: <a href="https://$repo">$repo</a></p>
       <p>Online Demo: <a href="https://$demo">$demo</a></p>
       <p>nDPI   Repo: <a href="https://$orig">$orig</a></p>
     </body>
   </html>
-EOT
+  EOT
 
 close $indexfh;
 
